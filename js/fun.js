7cff938cc94a52f9da2b4e1285a5f3ab97977d2d
EduardoDosSantosFerreira <eduardosferreira69@gmail.com>
2024-04-05 22:18:47 -0300

    update_v6

---
+++ js/fun.js
@@ -0,0 +1,72 @@
+document.getElementById('startQuizBtn').addEventListener('click', function () {
+  this.style.display = 'none';
+  document.getElementById('quizForm').style.display = 'block';
+  showQuestion(1); // Mostra a primeira pergunta
+});
+
+var questionNumber = 1;
+var totalQuestions = document.querySelectorAll('.form-group').length;
+
+document.querySelectorAll('.next-question-btn').forEach(button => {
+  button.addEventListener('click', function () {
+      showQuestion(++questionNumber);
+      if (questionNumber === totalQuestions) {
+          document.getElementById('submitBtn').style.display = 'block';
+      }
+  });
+});
+
+// Adicionando evento de clique aos elementos de entrada de rádio
+document.querySelectorAll('input[type=radio]').forEach(radio => {
+  radio.addEventListener('click', function () {
+      // Verificar se o usuário respondeu à última pergunta
+      if (questionNumber === totalQuestions) {
+          document.getElementById('submitBtn').style.display = 'block';
+      }
+      // Avançar para a próxima pergunta
+      showQuestion(++questionNumber);
+  });
+});
+
+function showQuestion(questionNumber) {
+  var questions = document.getElementsByClassName('form-group');
+  for (var i = 0; i < questions.length; i++) {
+      if (i === questionNumber - 1) {
+          questions[i].style.display = 'block';
+      } else {
+          questions[i].style.display = 'none';
+      }
+  }
+}
+
+document.getElementById('startQuizBtn').addEventListener('click', function () {
+  document.querySelector("h4").style.display = "none";
+  document.querySelectorAll("p").forEach(function (element) {
+      element.style.display = "none";
+  });
+  document.getElementById("quizForm").style.display = "block"; // Mostra o formulário do quiz
+});
+
+// Função para atualizar o perfil e exibir a imagem correspondente
+function updateProfileAndImage(maxProfile) {
+  document.getElementById('perfil').innerText = maxProfile;
+
+  // Atualiza a imagem correspondente ao perfil
+  var perfilImage = document.getElementById('perfilImage');
+  switch (maxProfile) {
+      case 'Executor':
+          perfilImage.src = '../img/executor.jpg';
+          break;
+      case 'Analista':
+          perfilImage.src = '../img/analista.jpg';
+          break;
+      case 'Comunicador':
+          perfilImage.src = '../img/comunicador.jpg';
+          break;
+      case 'Planejador':
+          perfilImage.src = '../img/planejador.jpg';
+          break;
+  }
+
+  document.getElementById('result').style.display = 'block';
+}
--- js/script.js
+++ js/script.js
@@ -4,54 +4,76 @@ document.getElementById('quizForm').addEventListener('submit', function(e) {
   const formData = new FormData(this);
   const responses = {};
   for (let pair of formData.entries()) {
-    responses[pair[0]] = pair[1];
+      responses[pair[0]] = pair[1];
   }
 
   const profileCounts = {
-    'Executor': 0,
-    'Analista': 0,
-    'Comunicador': 0,
-    'Planejador': 0
+      'Executor': 0,
+      'Analista': 0,
+      'Comunicador': 0,
+      'Planejador': 0
   };
 
   for (let response in responses) {
-    profileCounts[responses[response]]++;
+      profileCounts[responses[response]]++;
   }
 
   const maxProfile = Object.keys(profileCounts).reduce((a, b) => profileCounts[a] > profileCounts[b] ? a : b);
 
-  document.getElementById('perfil').innerText = maxProfile;
-  document.getElementById('result').style.display = 'block';
+  updateProfileAndImage(maxProfile);
 
   const ctx = document.getElementById('chart').getContext('2d');
   const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: Object.keys(profileCounts),
-      datasets: [{
-        label: 'Perfil',
-        data: Object.values(profileCounts),
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-        ],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
+      type: 'bar',
+      data: {
+          labels: Object.keys(profileCounts),
+          datasets: [{
+              label: 'Perfil',
+              data: Object.values(profileCounts),
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          }
       }
-    }
   });
 });
+
+function updateProfileAndImage(maxProfile) {
+  document.getElementById('perfil').innerText = maxProfile;
+
+  // Atualiza a imagem correspondente ao perfil
+  var perfilImage = document.getElementById('perfilImage');
+  switch (maxProfile) {
+      case 'Executor':
+          perfilImage.src = '../img/execultor.jpg';
+          break;
+      case 'Analista':
+          perfilImage.src = '../img/analista.jpg';
+          break;
+      case 'Comunicador':
+          perfilImage.src = '../img/comunicador.jpg';
+          break;
+      case 'Planejador':
+          perfilImage.src = '../img/planejador.jpg';
+          break;
+  }
+
+  document.getElementById('result').style.display = 'block';
+}
